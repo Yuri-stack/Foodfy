87c497067d48b91c9be6413a4430145e80367395
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2020-06-20 14:45:50 -0300

    Feature - Admin Area: details page design finished

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -7,19 +7,10 @@ exports.index = function(req, res){
     return res.render('admin/index', {recipes : data})
 }
 
-//Função para LISTAR as receitas na pag. Receitas
-exports.list = function(req, res){
-    return res.render('recipes', { recipes : data })
-}
-
-//Função para mostrar os detalhes das receitas
-exports.details = function(req, res){
-    const index = req.params.id;
-    const recipe = data[index]
-
-    // return res.render('details_recipe', { recipe : recipe })
-    return res.render('details_recipe', { recipe })
-}
+//Função para MOSTRAR os detalhes da receitas
+exports.show = function(req, res){
+    return res.render('admin/details_recipe', {recipe : data})
+} 
 
 
 
--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ routes.get('/recipes/:id', public.show)
 // //Rotas para a Administração
 routes.get("/admin/recipes", recipes.index);            // Mostrar a lista de receitas
 // routes.get("/admin/recipes/create", recipes.create);    // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id", recipes.show);         // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id", recipes.show);         // Exibir detalhes de uma receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit);    // Mostrar formulário de edição de receita
 // routes.post("/admin/recipes", recipes.post);            // Cadastrar nova receita
 // routes.put("/admin/recipes", recipes.put);              // Editar uma receita
