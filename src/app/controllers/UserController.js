6c7b50618be7357c675915a02d839eefb6a844fb
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-03-29 18:04:57 -0300

    Feat. Starting the Users structure

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    redirectCreate(req, res){
+        return res.render('admin/users/create.njk')
+    },
+
+    list(req, res){},
+
+    edit(req, res){},
+
+    post(req, res){},
+
+    put(req, res){},
+
+    delete(req, res){}
+
+}
--- src/routes/admin/users.routes.js
+++ src/routes/admin/users.routes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const routes = express.Router()
 
-// const User = require('../../app/controllers/UserController')
+const User = require('../../app/controllers/UserController')
 
 // Rotas para o Administrador gerenciar usuários
-// routes.get('/', User.list)                 // Mostrar a lista de usuários cadastrados
-// routes.post('/', User.post)                // Cadastrar um usuário
-// routes.get('/create', User.create)         // Mostrar o formulário de criação de um usuário
-// routes.put('/:id', User.put)               // Editar um usuário
-// routes.get('/:id/edit', User.edit)         // Mostrar o formulário de edição de um usuário
-// routes.delete('/:id', User.delete)         // Deletar um usuário
+// routes.get('/', User.list)                   // Mostrar a lista de usuários cadastrados
+routes.get('/create', User.redirectCreate)      // Mostrar o formulário de criação de um usuário
+// routes.get('/:id/edit', User.edit)           // Mostrar o formulário de edição de um usuário
+// routes.post('/', User.post)                  // Cadastrar um usuário
+// routes.put('/:id', User.put)                 // Editar um usuário
+// routes.delete('/:id', User.delete)           // Deletar um usuário
 
 module.exports = routes
